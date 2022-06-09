01cbd862eb764f475c6369b3f448f1e5f617ebf2
brijesh-2203 <brijesh.inexture@gmail.com>
2022-06-09 18:52:42 +0530

    09-06-2022

--- cloudConfig/cloudConfig/src/main/java/com/cloud/cloudConfig/CloudConfigApplication.java
+++ cloudConfig/cloudConfig/src/main/java/com/cloud/cloudConfig/CloudConfigApplication.java
@@ -1,16 +1,17 @@
-package com.dashboard.hystrixDashboard;
+package com.cloud.cloudConfig;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
 @EnableEurekaClient
-@EnableHystrixDashboard
-public class HystrixDashboardApplication {
+@EnableConfigServer
+public class CloudConfigApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HystrixDashboardApplication.class, args);
+		SpringApplication.run(CloudConfigApplication.class, args);
 	}
+
 }
